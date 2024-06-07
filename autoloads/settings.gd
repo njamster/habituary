2794ed3fb09376b0cd0b1e0fe15476759114d54a
njamster <njamster@users.noreply.github.com>
2024-06-07 15:40:03 +0200

    Use a different 'store_path' in DEBUG builds

--- autoloads/settings.gd
+++ autoloads/settings.gd
@@ -3,7 +3,18 @@ extends Node
 var date_format_show := "dddd, MMMM Do YYYY"
 var date_format_save := "YYYY-MM-DD"
 
-var home_path := OS.get_environment("USERPROFILE") if OS.has_feature("windows") else OS.get_environment("HOME")
-var store_path := home_path + "/" + str(ProjectSettings.get("application/config/name")).to_lower()
+var store_path:
+	get:
+		if OS.is_debug_build():
+			return "user://"
+		else:
+			var path := "/" + str(ProjectSettings.get("application/config/name")).to_snake_case()
+
+			if OS.has_feature("windows"):
+				path = OS.get_environment("USERPROFILE") + path
+			else:
+				path = OS.get_environment("HOME") + path
+
+			return path
 
 var save_delay := 6.0 # seconds
