fcf593bdea27035aaad5c1aaeb1e5a2d06bbca4c
njamster <njamster@users.noreply.github.com>
2024-08-04 22:54:47 +0200

    Rewrite the `day_start_hour_offset` change logic
    The code introduced by the last commit turned out to be flawed and still
    included bugs. So I gave it some more thought today and tried to simplify
    things as best as I could – hopefully it will work properly now!

--- autoloads/day_timer.gd
+++ autoloads/day_timer.gd
@@ -1,48 +1,58 @@
 @tool
 extends Timer
 
+const SECONDS_PER_DAY := 86_400
+const SECONDS_PER_HOUR := 3_600
+
 var today := Date.new():
 	set(value):
-		if Settings.current_day.as_dict() == today.as_dict():
-			Settings.current_day = value
-		today = value
-		EventBus.today_changed.emit()
+		if today.as_dict() != value.as_dict():
+			if Settings.current_day.as_dict() == today.as_dict():
+				Settings.current_day = value
+			today = value
+			EventBus.today_changed.emit()
 
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
 
-	self.start(_get_seconds_till_tomorrow())
-
 	timeout.connect(_on_new_day)
+
+	var current_time := Time.get_time_dict_from_system()
+	if current_time.hour * SECONDS_PER_HOUR + current_time.minute * 60 + current_time.second < \
+		Settings.day_start_hour_offset * SECONDS_PER_HOUR:
+			today = today.add_days(-1)
+
+	self.start(_get_seconds_till_tomorrow())
 
 	EventBus.day_start_hour_offset_changed.connect(_on_day_start_hour_offset_changed)
 
 
 func _get_seconds_till_tomorrow() -> int:
-	var seconds_till_tomorrow := 0
 	var current_time := Time.get_time_dict_from_system()
+	current_time.hour = wrapi(current_time.hour - Settings.day_start_hour_offset, 0, 24)
+
+	var seconds_till_tomorrow := 0
 	seconds_till_tomorrow += 60 - current_time.second
-	seconds_till_tomorrow += (60 - current_time.minute - 1) * 60
-	seconds_till_tomorrow += (24 + Settings.day_start_hour_offset - current_time.hour - 1) * 3600
+	seconds_till_tomorrow += (59 - current_time.minute) * 60
+	seconds_till_tomorrow += (23 - current_time.hour) * SECONDS_PER_HOUR
 	return seconds_till_tomorrow
 
 
 func _on_new_day() -> void:
 	today = today.add_days(1)
-
-	self.start(_get_seconds_till_tomorrow())
+	self.start(SECONDS_PER_DAY)
 
 
 func _on_day_start_hour_offset_changed(shift : int) -> void:
-	var new_time_till_day_start := self.wait_time + shift * 3600
+	var new_time_till_day_start := self.wait_time + shift * SECONDS_PER_HOUR
 
-	if new_time_till_day_start >= 86400:
-		# day start is 24 hours or more away => decrease today by one
+	if new_time_till_day_start > SECONDS_PER_DAY:
+		# new offset shifted day start more than 24 hours into the future => decrease today by one
 		today = today.add_days(-1)
-	elif new_time_till_day_start < 0:
-		# day start is already in the past => increase today by one
+	elif new_time_till_day_start <= 0:
+		# new offset shifted day start into the past => increase today by one
 		today = today.add_days(1)
 
 	self.start(_get_seconds_till_tomorrow())
