e84cff1dd4727fa703904a2d83f787956ab915d2
njamster <njamster@users.noreply.github.com>
2024-08-19 23:59:34 +0200

    Only show drag overlay when near the window border
    - That is, picking up an item in the center column via drag & drop will
      no longer bring up the drag overlay. If you move the mouse cursor to a
      x-position below 10% or above 90% of the window size while draggin an
      item, the respective overlay will be shown, though.

--- main_window/drag_overlay/drag_overlay.gd
+++ main_window/drag_overlay/drag_overlay.gd
@@ -2,6 +2,16 @@ extends CanvasLayer
 
 
 func _ready() -> void:
+	deactivate()
+
+
+func activate() -> void:
+	show()
+	set_process(true)
+
+
+func deactivate() -> void:
+	set_process(false)
 	hide()
 
 
@@ -9,6 +19,12 @@ func _notification(what: int) -> void:
 	match what:
 		NOTIFICATION_DRAG_BEGIN:
 			if get_viewport().gui_get_drag_data() is Array:
-				show()
+				activate()
 		NOTIFICATION_DRAG_END:
-			hide()
+			deactivate()
+
+
+func _process(_delta: float) -> void:
+	var window_size = get_window().size.x
+	$LeftBorder.visible = (get_parent().get_global_mouse_position().x < 0.1 * window_size)
+	$RightBorder.visible = (get_parent().get_global_mouse_position().x > 0.9 * window_size)
