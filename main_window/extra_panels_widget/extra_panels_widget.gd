c58b9cfb25a4d113578e14d7b38c848512912b01
njamster <njamster@users.noreply.github.com>
2024-09-07 13:05:10 +0200

    Highlight currently active ExtraPanel button
    That is, if the "Help", "Alarms" or "Capture" button is pressed, it will
    remain pressed until either it's pressed again (and the panel is closed)
    or another of those buttons is pressed (and a different panel opens).
    
    Also, switches the tooltip between "Show <Panel>" and "Hide <Panel>".

--- main_window/extra_panels_widget/extra_panels_widget.gd
+++ main_window/extra_panels_widget/extra_panels_widget.gd
@@ -1,13 +1,43 @@
 extends VBoxContainer
 
 
-func _on_capture_pressed() -> void:
+func _on_capture_toggled(toggled_on: bool) -> void:
+	if not($Alarms.button_pressed or $Help.button_pressed):
+		$Capture/Tooltip.hide_tooltip()
+
+	if toggled_on:
+		$Capture/Tooltip.text = $Capture/Tooltip.text.replace("Show", "Hide")
+		$Alarms.button_pressed = false
+		$Help.button_pressed = false
+	else:
+		$Capture/Tooltip.text = $Capture/Tooltip.text.replace("Hide", "Show")
+
 	EventBus.capture_button_pressed.emit()
 
 
-func _on_alarms_pressed() -> void:
+func _on_alarms_toggled(toggled_on: bool) -> void:
+	if not($Capture.button_pressed or $Help.button_pressed):
+		$Alarms/Tooltip.hide_tooltip()
+
+	if toggled_on:
+		$Alarms/Tooltip.text = $Alarms/Tooltip.text.replace("Show", "Hide")
+		$Capture.button_pressed = false
+		$Help.button_pressed = false
+	else:
+		$Alarms/Tooltip.text = $Alarms/Tooltip.text.replace("Hide", "Show")
+
 	EventBus.alarms_button_pressed.emit()
 
 
-func _on_help_pressed() -> void:
+func _on_help_toggled(toggled_on: bool) -> void:
+	if not($Capture.button_pressed or $Alarms.button_pressed):
+		$Help/Tooltip.hide_tooltip()
+
+	if toggled_on:
+		$Help/Tooltip.text = $Help/Tooltip.text.replace("Show", "Hide")
+		$Capture.button_pressed = false
+		$Alarms.button_pressed = false
+	else:
+		$Help/Tooltip.text = $Help/Tooltip.text.replace("Hide", "Show")
+
 	EventBus.help_button_pressed.emit()
