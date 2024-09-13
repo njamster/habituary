105b879867cd72e0c97ad174615b7dfe46759a4d
njamster <njamster@users.noreply.github.com>
2024-09-14 00:10:50 +0200

    Sort alarms in ascending order of their date
    - When an alarm is added to the list, it will be added below the last
      item with an equal or earlier date.
    - That means that items from the same day are sorted in the order they
      were added to the alarms list.
    - The list is still *not* persistent across restarts!
    
    Also, gives elements in the alarms list some visual upgrades:
    - Instead of a date, they now show the days until (or since) the event.
      (The date can stil be checked in the day countdown's tooltip)
    - The to-do's text is shown in a separate line below, using an ellipsis
      word wrap if it doesn't fit entirely.
    - Clicking on an element will jump to its associated date.

---
+++ main_window/side_panel/alarms/alarm/alarm.gd
@@ -0,0 +1,32 @@
+extends PanelContainer
+
+var date : Date:
+	set(value):
+		date = value
+		%Tooltip.text = date.format("MMM DD, YYYY")
+
+		var day_diff := date.day_difference_to(DayTimer.today)
+		if day_diff < 0:
+			modulate.a = 0.4
+			if day_diff == -1:
+				%DayCounter.text = "%d day ago" % abs(day_diff)
+			else:
+				%DayCounter.text = "%d days ago" % abs(day_diff)
+		elif day_diff > 0:
+			if day_diff == 1:
+				%DayCounter.text = "%d day left" % day_diff
+			else:
+				%DayCounter.text = "%d days left" % day_diff
+		else:
+			%DayCounter.add_theme_color_override("font_color", "ebcb8b")
+			%DayCounter.text = "Today"
+
+var text := "":
+	set(value):
+		text = value
+		%Heading.text = text
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+		Settings.current_day = date
--- main_window/side_panel/alarms/alarms.gd
+++ main_window/side_panel/alarms/alarms.gd
@@ -4,11 +4,11 @@ var FILE_PATH := Settings.store_path.path_join("alarms.txt")
 
 
 func _ready() -> void:
-	if FileAccess.file_exists(FILE_PATH):
-		var file = FileAccess.open(FILE_PATH, FileAccess.READ)
-		while file.get_position() < file.get_length():
-			_add_alarm(file.get_line())
-		file.close()
+	#if FileAccess.file_exists(FILE_PATH):
+		#var file = FileAccess.open(FILE_PATH, FileAccess.READ)
+		#while file.get_position() < file.get_length():
+			#_add_alarm(file.get_line())
+		#file.close()
 
 	EventBus.alarm_added.connect(_on_alarm_added)
 	EventBus.alarm_removed.connect(_on_alarm_removed)
@@ -19,32 +19,37 @@ func _ready() -> void:
 	)
 
 
-func _add_alarm(line : String) -> void:
-	var alarm := Label.new()
-	alarm.text = line
+func _add_alarm(date : Date, todo_text : String) -> void:
+	var alarm := preload("alarm/alarm.tscn").instantiate()
+	alarm.date = date
+	alarm.text = todo_text
 	$List.add_child(alarm)
+
+	for i in $List.get_child_count():
+		var child = $List.get_child(i)
+		if child.date.day_difference_to(date) > 0:
+			$List.move_child(alarm, i)
+			return
 
 
 func _on_alarm_added(to_do : Control) -> void:
 	# FIXME: avoid using a relative path that involves parent nodes
-	var date = to_do.get_node("../../../../..").date.format(Settings.date_format_save)
-	var alarm_text = "[%s] %s" % [date, to_do.text]
+	var date := Date.new(to_do.get_node("../../../../..").date.as_dict())
 
 	for alarm in $List.get_children():
-		if alarm.text == alarm_text:
+		if alarm.text == to_do.text and alarm.date.day_difference_to(date) == 0:
 			return
 
-	_add_alarm(alarm_text)
+	_add_alarm(date, to_do.text)
 	# TODO: save new state to disk
 
 
 func _on_alarm_removed(to_do : Control) -> void:
 	# FIXME: avoid using a relative path that involves parent nodes
-	var date = to_do.get_node("../../../../..").date.format(Settings.date_format_save)
-	var alarm_text = "[%s] %s" % [date, to_do.text]
+	var date := Date.new(to_do.get_node("../../../../..").date.as_dict())
 
 	for alarm in $List.get_children():
-		if alarm.text == alarm_text:
+		if alarm.text == to_do.text and alarm.date.day_difference_to(date) == 0:
 			alarm.queue_free()
 			# TODO: save new state to disk
 			return
