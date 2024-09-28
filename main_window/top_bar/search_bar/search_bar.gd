37e0dde981139f48bcbf75b0abaeff86ca5cee77
njamster <njamster@users.noreply.github.com>
2024-09-28 23:57:44 +0200

    Show shortcut hint in search bar while unfocused
    - That is, the shortcut hint will disappear as soon the search bar is
      focused, either by pressing CTRL+F or clicking the search bar, but
      reappears once the search bar gets unfocused again.
    - Like with the shortcuts displayed in the help panel, the shortcut text
      is directly derived from the InputMap - in this case: the first event
      associated with the "search_screen" input action.

--- main_window/top_bar/search_bar/search_bar.gd
+++ main_window/top_bar/search_bar/search_bar.gd
@@ -2,6 +2,8 @@ extends PanelContainer
 
 
 func _ready() -> void:
+	%ShortcutHint.text = InputMap.action_get_events("search_screen")[0].as_text().to_upper().replace("+", " + ")
+
 	EventBus.current_day_changed.connect(func(_current_day):
 		EventBus.search_query_changed.emit.call_deferred()
 	)
@@ -12,8 +14,20 @@ func _ready() -> void:
 	EventBus.dark_mode_changed.connect(func(dark_mode):
 		if dark_mode:
 			%Icon.self_modulate = Settings.NORD_06
+			for override in ["font_color", "font_hover_color", "font_pressed_color"]:
+				%ShortcutHint.add_theme_color_override(override, Settings.NORD_00)
+			var stylebox = %ShortcutHint.get_theme_stylebox("normal")
+			stylebox.bg_color = Settings.NORD_03
+			for style in ["normal", "hover", "pressed"]:
+				%ShortcutHint.add_theme_stylebox_override(style, stylebox)
 		else:
 			%Icon.self_modulate = Settings.NORD_00
+			for override in ["font_color", "font_hover_color", "font_pressed_color"]:
+				%ShortcutHint.add_theme_color_override(override, Settings.NORD_04)
+			var stylebox = %ShortcutHint.get_theme_stylebox("normal")
+			stylebox.bg_color = Settings.NORD_06
+			for style in ["normal", "hover", "pressed"]:
+				%ShortcutHint.add_theme_stylebox_override(style, stylebox)
 	)
 
 func _input(_event: InputEvent) -> void:
@@ -30,10 +44,12 @@ func _on_gui_input(event: InputEvent) -> void:
 
 func _on_search_query_focus_entered() -> void:
 	theme_type_variation = "SearchBar_Focused"
+	%ShortcutHint.hide()
 
 
 func _on_search_query_focus_exited() -> void:
 	theme_type_variation = "SearchBar"
+	%ShortcutHint.show()
 
 
 func _on_search_query_text_changed(new_text: String) -> void:
@@ -48,3 +64,7 @@ func _on_search_query_gui_input(event: InputEvent) -> void:
 
 func _on_search_query_text_submitted(_new_text: String) -> void:
 	%SearchQuery.release_focus()
+
+
+func _on_shortcut_hint_pressed() -> void:
+	%SearchQuery.grab_focus()
