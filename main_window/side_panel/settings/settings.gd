12859068451f8c412d7ccfa472b7b90e9ea0b54f
njamster <njamster@users.noreply.github.com>
2024-11-16 23:59:15 +0100

    Rework settings UI, pt. 1
    - Improves tree structure by renaming the VBoxContainers holding the
      individual settings from the generic "VBox<X>" to the actual name of
      the setting, and makes them accessible as unique names.
    - This simplifies the process of getting the node path to a setting's
      explanation text to just "%<SettingName>/Explanation".
    - Replaces the (uneditable) LineEdit showing `Settings.store_path` with
      a button that will open the directory in the users file manager.
    - Turns the store path explanation into a RichTextLabel, that now does
      highlight the recommendation to make backups in bold.
    - Stops calling the setting's `_on_selected|changed|toggled` callbacks
      in `_ready` to set their initial explanation text, and directly sets
      those in the scene file for a more representative preview instead.
    - Removes the previous workaround of setting the anchors and offsets of
      the first explanation text to prevent it from blowing up the size of
      the panel, as it seems no longer necessary with the settings now being
      part of the (scrollable) side panel instead of its own one.
    - Reduces the vertical padding between individual settings by 25%.
    - Sets all labels in a settings title to the "Shrink End" size flag.
    - Rewords some of the explanation texts slightly.

--- main_window/side_panel/settings/settings.gd
+++ main_window/side_panel/settings/settings.gd
@@ -1,55 +1,49 @@
 extends VBoxContainer
 
 
-func _enter_tree() -> void:
-	# See: https://github.com/godotengine/godot/issues/83546#issuecomment-1856927502
-	# Without this line (or a custom_minimum_size for the label), the autowrapping will blow up the
-	# vertical size of the panel. This can (as of now) only be done via code, not in the editor.
-	$ScrollContainer/PaddedScrollbar/VBox/VBox1/Explanation.set_anchors_and_offsets_preset(Control.PRESET_TOP_WIDE)
-
-
 func _ready() -> void:
-	%StorePath/Path.text = Settings.store_path
-
 	for option_name in Settings.TodayPosition:
 		var option_id : int = Settings.TodayPosition[option_name]
-		%TodayPosition/Options.add_item(option_name.capitalize(), option_id)
+		%TodayPosition/Setting/Options.add_item(option_name.capitalize(), option_id)
 		if option_id == Settings.today_position:
-			%TodayPosition/Options.select(option_id)
-			_on_options_item_selected(option_id)
-#
-	%FirstWeekday/Options.button_pressed = Settings.start_week_on_monday
-	_on_options_toggled(Settings.start_week_on_monday)
+			%TodayPosition/Setting/Options.select(option_id)
 
-	%DayStartsAt/Options.value = Settings.day_start_hour_offset
-	_on_options_value_changed(Settings.day_start_hour_offset)
+	%FirstWeekday/Setting/Options.button_pressed = Settings.start_week_on_monday
+
+	%DayStart/Setting/Options.value = Settings.day_start_hour_offset
 
 
-func _on_options_item_selected(index: int) -> void:
+func _on_change_store_path_pressed() -> void:
+	OS.shell_show_in_file_manager(Settings.store_path)
+
+
+func _on_today_position_item_selected(index: int) -> void:
 	Settings.today_position = Settings.TodayPosition[Settings.TodayPosition.keys()[index]]
+
 	match index:
 		Settings.TodayPosition.LEFTMOST:
-			%TodayPosition.get_node("../Explanation").text = \
-				"The current day will be displayed at the leftmost position of the list view."
+			%TodayPosition/Explanation.text = \
+				"After changing dates, the currently selected day will be displayed at the leftmost position of the list view."
 		Settings.TodayPosition.SECOND_PLACE:
-			%TodayPosition.get_node("../Explanation").text = \
-				"The current day will be displayed at the second position of the list view."
+			%TodayPosition/Explanation.text = \
+				"After changing dates, the currently selected day will be displayed at the second position of the list view."
 		Settings.TodayPosition.CENTERED:
-			%TodayPosition.get_node("../Explanation").text = \
-				"The current day will be displayed at the center position of the list view."
+			%TodayPosition/Explanation.text = \
+				"After changing dates, the currently selected day will be displayed at the center position of the list view."
 		_:
-			%TodayPosition.get_node("../Explanation").text = ""
+			%TodayPosition/Explanation.text = ""
 
 
-func _on_options_toggled(toggled_on: bool) -> void:
+func _on_first_weekday_toggled(toggled_on: bool) -> void:
 	Settings.start_week_on_monday = toggled_on
+
 	if toggled_on:
-		%FirstWeekday.get_node("../Explanation").text = "New weeks in the calendar widget will start on Mondays."
+		%FirstWeekday/Explanation.text = "New weeks in the calendar widget start on Mondays."
 	else:
-		%FirstWeekday.get_node("../Explanation").text = "New weeks in the calendar widget will start on Sundays."
+		%FirstWeekday/Explanation.text = "New weeks in the calendar widget start on Sundays."
 
 
-func _on_options_value_changed(value: int) -> void:
-	Settings.day_start_hour_offset = value
-	%DayStartsAt.get_node("../Explanation").text = \
-		"Today's date will shift one day forward at %02d:00." % value
+func _on_day_start_value_changed(value: float) -> void:
+	Settings.day_start_hour_offset = int(value)
+
+	%DayStart/Explanation.text = "Today's date will shift one day forward at %02d:00." % value
