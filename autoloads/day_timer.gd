329179177fe00287f96b87713e595f5cba9dff89
njamster <njamster@users.noreply.github.com>
2024-11-20 23:55:24 +0100

    Fix `_on_day_start_changed`
    - The previous logic was entirely wrong! I just didn't notice it because
      a sequence error in `_get_seconds_till_tomorrow` made it work for my
      specific test case nonetheless...
    - That sequence error (doing `current_time.hour -= 1` after the `wrapi`
      line for `current_time.hour` instead of before) is now fixed!
    - The logic of `_on_day_start_changed` has been rewritten based on the
      logic that was already present in `_ready` previously. Consequently,
      `_ready` now simply calls `_on_day_start_changed` as well.
    - To make this work, a new parameter `called_from_ready` (defaulting to
      `false`) was added, which avoids increasing `today` there.
    - Also, there's a new `_is_offset` variable now, to avoid offsetting
      `today` multiple times on repeated adjustments of the offset.
    - The order of methods was slightly altered, to better reflect the order
      in which they are called.

--- autoloads/day_timer.gd
+++ autoloads/day_timer.gd
@@ -13,6 +13,8 @@ var today := Date.new():
 			Settings.bookmarks_due_today = 0
 			EventBus.today_changed.emit()
 
+var _is_offset := false
+
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
@@ -20,14 +22,30 @@ func _ready() -> void:
 
 	timeout.connect(_on_new_day)
 
+	EventBus.day_start_changed.connect(_on_day_start_changed)
+
+	_on_day_start_changed(true)
+
+
+func _on_new_day() -> void:
+	today = today.add_days(1)
+	self.start(SECONDS_PER_DAY)
+
+
+func _on_day_start_changed(called_from_ready := false) -> void:
 	var current_time := Time.get_time_dict_from_system()
+
 	if current_time.hour * SECONDS_PER_HOUR + current_time.minute * 60 + current_time.second < \
 		Settings.day_start_hour_offset * SECONDS_PER_HOUR + Settings.day_start_minute_offset * 60:
-			today = today.add_days(-1)
+			if not _is_offset:
+				today = today.add_days(-1)
+				_is_offset = true
+	elif not called_from_ready:
+		if _is_offset:
+			today = today.add_days(1)
+			_is_offset = false
 
 	self.start(_get_seconds_till_tomorrow())
-
-	EventBus.day_start_changed.connect(_on_day_start_changed)
 
 
 func _get_seconds_till_tomorrow() -> int:
@@ -35,9 +53,9 @@ func _get_seconds_till_tomorrow() -> int:
 
 	# Adjust current_time relative to the (potentially offset) day start, e.g.:
 	# current_time == 1:13 with day start == 2:17 => current_time (adjusted) == 22:56
-	current_time.hour = wrapi(current_time.hour - Settings.day_start_hour_offset, 0, 24)
 	if current_time.minute - Settings.day_start_minute_offset < 0:
 		current_time.hour -= 1
+	current_time.hour = wrapi(current_time.hour - Settings.day_start_hour_offset, 0, 24)
 	current_time.minute = wrapi(current_time.minute - Settings.day_start_minute_offset, 0, 60)
 
 	# Now simply calculate the seconds till the end of the day, using that current_time value:
@@ -50,22 +68,3 @@ func _get_seconds_till_tomorrow() -> int:
 	seconds_till_tomorrow += (24 - current_time.hour - 1) * SECONDS_PER_HOUR
 
 	return seconds_till_tomorrow
-
-
-func _on_new_day() -> void:
-	today = today.add_days(1)
-	self.start(SECONDS_PER_DAY)
-
-
-func _on_day_start_changed() -> void:
-	var old_time_left := self.time_left
-	var new_time_left := _get_seconds_till_tomorrow()
-
-	if old_time_left < SECONDS_PER_DAY and new_time_left > SECONDS_PER_DAY:
-		# the new offset shifted the day start above 24 hours into the future => decrease today
-		today = today.add_days(-1)
-	elif old_time_left > SECONDS_PER_DAY and new_time_left < SECONDS_PER_DAY:
-		# the new offset shifted the  day start below 24 hours into the future => increase today
-		today = today.add_days(1)
-
-	self.start(new_time_left)
