f33ad34b88aad2849a8470495461b51abf541a63
njamster <njamster@users.noreply.github.com>
2025-02-16 23:59:28 +0100

    Turn ToggleButton into a tool script
    ... to allow properly previewing the icons inside the editor.
    
    Also, includes a custom `_set` function, which prevents the user from
    changing the `toggle_mode` or `icon` properties directly. The former
    is assumed to be always active in this node, while the latter is only
    supposed to be changed indirectly through `icon_toggled_off|on`.

--- custom/toggle_button.gd
+++ custom/toggle_button.gd
@@ -1,19 +1,40 @@
+@tool
 extends Button
 class_name ToggleButton
 
-@export var icon_toggled_off : Texture2D
-@export var icon_toggled_on : Texture2D
+
+@export var icon_toggled_off: Texture2D:
+	set(value):
+		icon_toggled_off = value
+		_on_toggled()
+
+@export var icon_toggled_on: Texture2D:
+	set(value):
+		icon_toggled_on = value
+		_on_toggled()
 
 
 func _ready() -> void:
+	_set_initial_state()
+	_connect_signals()
+
+
+func _set_initial_state() -> void:
 	toggle_mode = true
-
-	self.toggled.connect(_on_toggled)
-	_on_toggled(button_pressed)
+	_on_toggled()
 
 
-func _on_toggled(toggled_on : bool) -> void:
-	if toggled_on:
-		self.icon = icon_toggled_on
-	else:
-		self.icon = icon_toggled_off
+func _connect_signals() -> void:
+	toggled.connect(_on_toggled)
+
+
+func _on_toggled(toggled_on := button_pressed) -> void:
+	icon = icon_toggled_on if toggled_on else icon_toggled_off
+
+
+func _set(property: StringName, _value: Variant) -> bool:
+	if property == "toggle_mode" or property == "icon":
+		# the user is not supposed to change these properties directly
+		return true
+
+	return false
