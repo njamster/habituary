be31106ae2e9110f426cbd767d6486f6eed632dc
njamster <njamster@users.noreply.github.com>
2025-02-20 17:20:47 +0100

    Fold headings based on indentation where possible
    That is, if a heading has any sub items, folding it will only fold away
    those, not all items until the next heading (if there is any).
    If a heading has no sub items, it falls back to the old way of folding.
    
    Also, the code accounts for nested headings (a heading with at least one
    sub item that's a heading as well) now, too. If the sub item heading was
    folded when folding its parent, it will be restored as folded as soon as
    the parent gets unfolded again. Unlike before, a todo will no longer get
    folded when the folded heading has a higher indentation level.
    
    Lastly, headings are no longer included in the item count of a heading
    (which is displayed while the heading is folded).

--- main_window/list_view/day_panel/scrollable_todo_list/todo_list/todo_list.gd
+++ main_window/list_view/day_panel/scrollable_todo_list/todo_list/todo_list.gd
@@ -191,27 +191,40 @@ func find_item_pos(at_position : Vector2) -> int:
 	return %Items.get_child_count()
 
 
-func fold_heading(item_index : int, unfold := false) -> void:
+func fold_heading(item_index: int, unfold := false) -> void:
 	var heading := %Items.get_child(item_index)
+
+	var has_sub_items := false
+	if item_index + 1 < %Items.get_child_count():
+		var child := %Items.get_child(item_index + 1)
+		if child.indentation_level > heading.indentation_level:
+			has_sub_items = true
 
 	var num_items := 0
 	var num_done := 0
 
 	for i in range(item_index + 1, %Items.get_child_count()):
 		var child := %Items.get_child(i)
-		if child.is_heading:
-			heading.set_extra_info(num_done, num_items)
-			return
-		else:
-			if unfold:
-				if Settings.hide_ticked_off_todos:
-					if child.state == child.States.TO_DO or \
-						child._has_unticked_sub_todos():
-							child.show()
-				else:
-					child.show()
+
+		if child.indentation_level < heading.indentation_level:
+			break  # the for-loop
+		elif has_sub_items or child.is_heading:
+			if child.indentation_level == heading.indentation_level:
+				break  # the for-loop
+
+		if unfold:
+			if Settings.hide_ticked_off_todos:
+				if child.state == child.States.TO_DO or \
+					child._has_unticked_sub_todos():
+						child.show()
 			else:
-				child.hide()
+				child.show()
+				if child.is_heading and child.is_folded:
+					fold_heading.call_deferred(child.get_index())
+		else:
+			child.hide()
+
+		if not child.is_heading:
 			num_items += 1
 			if child.state != child.States.TO_DO:
 				num_done += 1
