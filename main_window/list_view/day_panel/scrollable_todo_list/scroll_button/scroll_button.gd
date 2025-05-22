njamster/habituary

commit e17df1278e8ecc5080c8618854d08f4d49d1c98c

Only allow focusing the edit field via left-clicks
By default, pressing the right or middle mouse button would have worked
as well. This commit changes that by calling `accept_event` for those
input events while (intentionally) doing anything else.

Also, this merges the `on_gui_input` callback into `_on_edit_gui_input`.
Previously, the item's own `gui_input` signal was connected to it as
well, but that no longer served a purpose in the current version.

main_window/list_view/day_panel/scrollable_todo_list/todo_list/todo_item/todo_item.gd
@@ -216,7 +216,6 @@ func _connect_signals() -> void:
 
 	#region Local Signals
 	focus_exited.connect(_on_focus_exited)
-	gui_input.connect(_on_gui_input)
 	tree_exiting.connect(_on_tree_exiting)
 
 	%MainRow.mouse_entered.connect(_on_mouse_entered)
@@ -225,8 +224,6 @@ func _connect_signals() -> void:
 	%CheckBox.gui_input.connect(_on_check_box_gui_input)
 
 	%FoldHeading.toggled.connect(_on_fold_heading_toggled)
-
-	%Edit.gui_input.connect(_on_gui_input)
 
 	%Edit.text_changed.connect(_on_edit_text_changed)
 	%Edit.text_submitted.connect(_on_edit_text_submitted)
@@ -711,11 +708,6 @@ func _on_bookmark_indicator_gui_input(event: InputEvent) -> void:
 		EventBus.bookmark_indicator_clicked.emit(date, get_list_index())
 
 
-func _on_gui_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		%Edit.release_focus()
-
-
 # NOTE: `tree_exiting` will be emitted both when this panel is about to be removed from the tree
 # because the user scrolled the list' view, as well as on a NOTIFICATION_WM_CLOSE_REQUEST, but also
 # when an item is deleted (which is why we check if it's queded for deletion first).
@@ -780,8 +772,17 @@ func _apply_state_relative_formatting(immediate := false) -> void:
 
 
 func _on_edit_gui_input(event: InputEvent) -> void:
+	# Only allow focusing the edit field via left-clicks (by default, pressing
+	# the right or middle mouse button will work as well)
+	if event is InputEventMouseButton and \
+			event.button_index != MOUSE_BUTTON_LEFT:
+		accept_event()
+
 	if event.is_action_pressed("ui_text_backspace") and %Edit.text == "":
 		get_item_list().unindent_todo(self)
+
+	if event.is_action_pressed("ui_cancel"):
+		%Edit.release_focus()
 
 
 func _has_unticked_sub_todos() -> bool:

commit 712e23ac29b347040f6fa9eb325e66520fc0ef36

Fix several scrolling issues
First, fix a bug introduced by e17df12, which not only consumed right
and middle mouse button inputs, but scroll wheel inputs as well! That
made it impossible to scroll a to-do list while hovering the mouse over
a to-do's "Edit" field (which takes up most of a the space).

Secondly, add a 13-pixel high spacer element to the end of each item
list, which forces the scroll container to scroll down until the next
line element, and not just to the end of the to-do's text.

Thirdly, remove the ScrollButton's (line) texture while it's in UP-mode.
Since each TodoList adds a 7-pixel offset to their lines, the next line
was still visible slightly underneath the button, which made it appear
as a duplicate. The offset ensures that the line remains clickable, so
one can add a to-do above the topmost visible one without scrolling.

Lastly, adjust the `_scroll_one_item_up|down` methods to *only* scroll
in full increments of `TODO_ITEM_HEIGHT`. This stops a to-do list from
slightly shifting vertically when scrolling all to the end.

main_window/list_view/day_panel/scrollable_todo_list/scroll_button/scroll_button.gd
@@ -2,7 +2,14 @@ extends TextureRect
 
 
 enum Modes {UP, DOWN}
-@export var mode := Modes.UP
+@export var mode := Modes.UP:
+	set(value):
+		mode = value
+
+		if mode == Modes.UP:
+			texture = null
+		else:
+			texture = preload("../images/line.png")
 
 @onready var SCROLL_CONTAINER := get_parent().get_node("ScrollContainer")
 @onready var SCROLL_CONTAINER_CONTENT := SCROLL_CONTAINER.get_child(0)

main_window/list_view/day_panel/scrollable_todo_list/scroll_button/scroll_button.tscn
@@ -1,6 +1,5 @@
-[gd_scene load_steps=4 format=3 uid="uid://ofa44bt1yn4u"]
+[gd_scene load_steps=3 format=3 uid="uid://ofa44bt1yn4u"]
 
-[ext_resource type="Texture2D" uid="uid://mvrb0206nsy5" path="res://main_window/list_view/day_panel/scrollable_todo_list/images/line.png" id="2_6ekag"]
 [ext_resource type="Script" uid="uid://bbwswrn7qu1tp" path="res://main_window/list_view/day_panel/scrollable_todo_list/scroll_button/scroll_button.gd" id="2_gh5kw"]
 [ext_resource type="Script" uid="uid://bcgem14qd6rmk" path="res://custom/drag_hover_trigger.gd" id="3_bvify"]
 
@@ -10,7 +9,6 @@ offset_right = 1.0
 offset_bottom = 40.0
 mouse_filter = 0
 mouse_default_cursor_shape = 2
-texture = ExtResource("2_6ekag")
 stretch_mode = 1
 script = ExtResource("2_gh5kw")
 

main_window/list_view/day_panel/scrollable_todo_list/scroll_container/scroll_container.gd
@@ -35,17 +35,21 @@ func _on_gui_input(event: InputEvent) -> void:
 func _scroll_one_item_down(emit_scrolled_signal := true) -> void:
 	var previous_value := scroll_vertical
 	scroll_vertical += TODO_ITEM_HEIGHT
-	if emit_scrolled_signal:
-		if scroll_vertical != previous_value:
-			scrolled.emit.call_deferred()
+
+	if scroll_vertical != previous_value + TODO_ITEM_HEIGHT:
+		scroll_vertical = previous_value
+	elif emit_scrolled_signal and scroll_vertical != previous_value:
+		scrolled.emit.call_deferred()
 
 
 func _scroll_one_item_up(emit_scrolled_signal := true) -> void:
 	var previous_value := scroll_vertical
 	scroll_vertical -= TODO_ITEM_HEIGHT
-	if emit_scrolled_signal:
-		if scroll_vertical != previous_value:
-			scrolled.emit.call_deferred()
+
+	if scroll_vertical != previous_value - TODO_ITEM_HEIGHT:
+		scroll_vertical = previous_value
+	elif emit_scrolled_signal and scroll_vertical != previous_value:
+		scrolled.emit.call_deferred()
 
 
 func _set(property: StringName, value: Variant) -> bool:

main_window/list_view/day_panel/scrollable_todo_list/scrollable_todo_list.tscn
@@ -15,6 +15,7 @@ size_flags_vertical = 3
 script = ExtResource("1_qun0k")
 
 [node name="ScrollUpButton" parent="." instance=ExtResource("1_6pw8l")]
+custom_minimum_size = Vector2(0, 40)
 layout_mode = 2
 
 [node name="ScrollContainer" type="ScrollContainer" parent="."]
@@ -30,5 +31,6 @@ unique_name_in_owner = true
 layout_mode = 2
 
 [node name="ScrollDownButton" parent="." instance=ExtResource("1_6pw8l")]
+custom_minimum_size = Vector2(0, 40)
 layout_mode = 2
 mode = 1

main_window/list_view/day_panel/scrollable_todo_list/todo_list/item_list.gd
@@ -59,7 +59,10 @@ func _on_gui_input(event: InputEvent) -> void:
 
 
 #region Adding Items
-func _on_item_added(item: ToDoItem) -> void:
+func _on_item_added(item: Node) -> void:
+	if item != ToDoItem:
+		return
+
 	if name == "Items":
 		item.indentation_level = 0
 	elif name == "SubItems":

main_window/list_view/day_panel/scrollable_todo_list/todo_list/todo_item/todo_item.gd
@@ -784,7 +784,7 @@ func _on_edit_gui_input(event: InputEvent) -> void:
 	# Only allow focusing the edit field via left-clicks (by default, pressing
 	# the right or middle mouse button will work as well)
 	if event is InputEventMouseButton and \
-			event.button_index != MOUSE_BUTTON_LEFT:
+			event.button_index in [MOUSE_BUTTON_MIDDLE, MOUSE_BUTTON_RIGHT]:
 		accept_event()
 
 	if event.is_action_pressed("ui_text_backspace") and %Edit.text == "":

main_window/list_view/day_panel/scrollable_todo_list/todo_list/todo_list.gd
@@ -17,6 +17,12 @@ func _ready() -> void:
 func _set_initial_state() -> void:
 	hide_line_highlight()
 
+	var spacer = Control.new()
+	spacer.name = "Spacer"
+	spacer.custom_minimum_size.y = 13
+	spacer.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	%Items.add_child(spacer, true, Node.INTERNAL_MODE_BACK)
+
 
 func _connect_signals() -> void:
 	#region Parent Signals
@@ -83,7 +89,7 @@ func show_line_highlight(x_offset: int) -> void:
 	%LineHighlight.position.y = clamp(
 		line_position - 2,
 		0,
-		$Items.get_combined_minimum_size().y
+		$Items.get_combined_minimum_size().y - %Items.get_node("Spacer").get_combined_minimum_size().y
 	) + $Offset.get_theme_constant("margin_top")
 
 	%LineHighlight.modulate.a = 1.0