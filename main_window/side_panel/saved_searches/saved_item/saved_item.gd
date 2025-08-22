njamster/habituary

commit 7c11eeae62898c5c4fd9e2dfe9904e609f8507e3

Fix appearance when there are no matching items
Before, the second row of a saved item without any matches simply read
"Latest Match:" (followed by nothing). Now, it will show "No matching
items" instead, and change the font to italic for that.

Also, ensures the icon of the "Add Alarm" button remains at the right
side of the item no matter what by toggling its EXTEND flag.

main_window/side_panel/saved_searches/saved_item/saved_item.gd
@@ -7,8 +7,8 @@ signal save_requested
 var date : Date:
 	set(value):
 		date = value
+		_update_day_counter()
 		if date:
-			_update_day_counter()
 			_update_third_row()
 
 var text := "":
@@ -60,7 +60,6 @@ func _find_last_mention() -> Date:
 				return Date.from_string(key)
 			line_id += 1
 
-	%DayCounter.hide()
 	return null
 
 
@@ -103,6 +102,19 @@ func _connect_signals() -> void:
 
 
 func _update_day_counter() -> void:
+	if not date:
+		%DayCounter.hide()
+		%LatestMatch.text = "No matching items"
+		%LatestMatch.add_theme_font_override(
+			"font",
+			preload("res://theme/fonts/OpenSans-MediumItalic.ttf")
+		)
+		return  # early
+	else:
+		%DayCounter.show()
+		%LatestMatch.text = "Latest Match:"
+		%LatestMatch.remove_theme_font_override("font")
+
 	day_diff = date.day_difference_to(DayTimer.today)
 
 	if Settings.use_relative_saved_search_dates:

main_window/side_panel/saved_searches/saved_item/saved_item.tscn
@@ -67,6 +67,7 @@ layout_mode = 2
 theme_type_variation = &"TinySeparation"
 
 [node name="LatestMatch" type="Label" parent="VBox/SecondRow"]
+unique_name_in_owner = true
 layout_mode = 2
 text = "Latest Match:"
 
@@ -88,7 +89,7 @@ text_alignment = 2
 unique_name_in_owner = true
 custom_minimum_size = Vector2(28, 28)
 layout_mode = 2
-size_flags_horizontal = 8
+size_flags_horizontal = 10
 size_flags_vertical = 0
 focus_mode = 0
 mouse_default_cursor_shape = 2

commit d1ed6737166f33011753b78606d31793b596c1bf

Resort saved searches if match date changes
Previously the order would only update when `load_from_disk` was called,
but remain as it was if the date of the last matching item changed (e.g.
because an item with a matching text got added or deleted).

When `load_from_disk` is called, old items are now immediately free'd
(instead of using `queue_free`) to reduce the number of items to sort.
Furthermore, `_resort_list()` will be skipped now if the number of items
is less than two (since there's nothing to sort then).

Also, fixes a bug introduced by the previous commit 7c11eea: Turns out
that toggling the EXTEND flag of the "Add Alarm" button was *not* the
right solution, as this will steal space from the DayCounter.
The DayCounter text is now set to the empty string instead of hiding it
when there's no date set, so its own EXTEND flag remains active.

main_window/side_panel/saved_searches/saved_item/saved_item.gd
@@ -2,6 +2,7 @@ extends PanelContainer
 
 
 signal save_requested
+signal resort_requested
 
 
 var date : Date:
@@ -19,7 +20,11 @@ var text := "":
 var warning_threshold := Utils.MIN_INT
 
 # used for sorting
-var day_diff := Utils.MIN_INT
+var day_diff := Utils.MIN_INT:
+	set(value):
+		if day_diff != value:
+			day_diff = value
+			resort_requested.emit()
 var line_id : int
 
 
@@ -103,15 +108,16 @@ func _connect_signals() -> void:
 
 func _update_day_counter() -> void:
 	if not date:
-		%DayCounter.hide()
+		%DayCounter.text = ""
 		%LatestMatch.text = "No matching items"
 		%LatestMatch.add_theme_font_override(
 			"font",
 			preload("res://theme/fonts/OpenSans-MediumItalic.ttf")
 		)
+		day_diff = Utils.MIN_INT
 		return  # early
 	else:
-		%DayCounter.show()
+		%DayCounter.text = ""
 		%LatestMatch.text = "Latest Match:"
 		%LatestMatch.remove_theme_font_override("font")
 

main_window/side_panel/saved_searches/saved_item/saved_item.tscn
@@ -83,13 +83,13 @@ text_overrun_behavior = 3
 [node name="Tooltip" type="Node" parent="VBox/SecondRow/DayCounter"]
 unique_name_in_owner = true
 script = ExtResource("2_5q2o3")
-text_alignment = 2
+popup_position = 2
 
 [node name="Alarm" type="Button" parent="VBox/SecondRow"]
 unique_name_in_owner = true
 custom_minimum_size = Vector2(28, 28)
 layout_mode = 2
-size_flags_horizontal = 10
+size_flags_horizontal = 8
 size_flags_vertical = 0
 focus_mode = 0
 mouse_default_cursor_shape = 2

main_window/side_panel/saved_searches/saved_searches.gd
@@ -29,7 +29,7 @@ func _connect_signals() -> void:
 func _load_from_disk() -> void:
 	# remove previous entries (if there are any)
 	for item in %Items.get_children():
-		item.queue_free()
+		item.free()
 
 	# add new entries based on the cached lines
 	if "saved_searches" in Cache.data:
@@ -48,13 +48,12 @@ func _load_from_disk() -> void:
 				)
 			else:
 				new_item.text = raw_line
-			%Items.add_child(new_item)
-
 			new_item.save_requested.connect(func():
 				print("[DEBUG] Saved Search Save Requested: (Re)Starting DebounceTimer...")
 				$DebounceTimer.start()
 			)
-		_resort_list()
+			new_item.resort_requested.connect(_resort_list)
+			%Items.add_child(new_item)
 
 		$NoneSaved.hide()
 	else:
@@ -63,6 +62,9 @@ func _load_from_disk() -> void:
 
 func _resort_list() -> void:
 	var items := %Items.get_children()
+
+	if items.size() < 2:
+		return  # early
 
 	# Sort bookmarks...
 	items.sort_custom(func(a, b):