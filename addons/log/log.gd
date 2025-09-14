f64f390b665d682c4edb317e83e486800902f4df
njamster <njamster@users.noreply.github.com>
2025-09-14 22:11:49 +0200

    Use passed time since startup for log messages
    ... instead of simply querying the system time. Not only does that
    enable timing messages with sub-second precision, it's also safe
    against sudden changes of the system time during runtime.

--- addons/log/log.gd
+++ addons/log/log.gd
@@ -27,11 +27,21 @@ func _log(level, message) -> void:
 	print_rich(
 		"[color={color}]{time} [b][{level}][/b]: {message}[/color]".format({
 			"color": _colors[level],
-			"time": Time.get_datetime_string_from_system(false, true),
+			"time": get_current_timestamp(),
 			"level": Level.keys()[level],
 			"message": message
 		})
 	)
+
+
+func get_current_timestamp() -> String:
+	var microseconds_since_startup := Time.get_ticks_usec()
+	return "%02d:%02d:%02d.%-6d" % [
+		microseconds_since_startup / 3_600_000_000,  # hours
+		microseconds_since_startup / 60_000_000,  # minutes
+		microseconds_since_startup / 1_000_000,  # seconds
+		microseconds_since_startup % 1_000_000  # microseconds
+	]
 
 
 func debug(message) -> void:
