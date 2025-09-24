dbbb6d8e26b4633496b2af1e20d81b121232320f
njamster <njamster@users.noreply.github.com>
2025-09-24 17:38:50 +0200

    Restructure Help panel using FoldableContainers
    The sections mostly remained the same. The "WHILE EDITING A TO-DO" one
    was moved to the beginning of the list, as for most users it's likely
    the most relevant one. `toggle_fullscreen` and `toggle_dark_mode` were
    moved to their own section ("CHANGE LOOK"). Also, the wording of some
    descriptions has been shortened where the context allowed for it.

--- main_window/side_panel/help/help.gd
+++ main_window/side_panel/help/help.gd
@@ -3,26 +3,6 @@ extends VBoxContainer
 const INPUT_HINT_SCENE := preload("input_hint/input_hint.tscn")
 
 var descriptions := {
-	"view_mode_1": "Change View To Show 1 Day",
-	"view_mode_2": "Change View To Show 3 Days",
-	"view_mode_3": "Change View To Show 5 Days",
-	"view_mode_4": "Change View To Show 7 Days",
-
-	"shift_view_backward": "Shift 1 View Backward",
-	"previous_day": "Shift 1 Day Backward",
-	"jump_to_today": "Jump To Today",
-	"next_day": "Shift 1 Day Forward",
-	"shift_view_forward": "Shift 1 View Forward",
-
-	"toggle_calendar_widget": "Open/Close Calendar Widget",
-	"search_screen": "Search To-Dos On Screen",
-	"show_help": "Open/Close Help Panel",
-	"show_bookmarks": "Open/Close Bookmarks Panel",
-	"show_capture": "Open/Close Capture Panel",
-	"show_settings": "Open/Close Settings Panel",
-	"toggle_fullscreen": "Toggle Fullscreen On/Off",
-	"toggle_dark_mode": "Toggle Dark Mode On/Off",
-
 	"toggle_todo": "Toggle Done State",
 	"cancel_todo": "Toggle Canceled State",
 	"indent_todo": "Indent To-Do",
@@ -37,62 +17,46 @@ var descriptions := {
 	"next_todo": "Select Next To-Do",
 	"move_todo_up": "Move To-Do Up",
 	"move_todo_down": "Move To-Do Down",
+
+	"view_mode_1": "Show 1 Day",
+	"view_mode_2": "Show 3 Days",
+	"view_mode_3": "Show 5 Days",
+	"view_mode_4": "Show 7 Days",
+
+	"shift_view_backward": "1 View Backward",
+	"previous_day": "1 Day Backward",
+	"jump_to_today": "Jump To Today",
+	"next_day": "1 Day Forward",
+	"shift_view_forward": "1 View Forward",
+
+	"search_screen": "On Screen",
+
+	"toggle_calendar_widget": "Calendar",
+	"show_help": "Help",
+	"show_bookmarks": "Bookmarks",
+	"show_capture": "Capture",
+	"show_settings": "Settings",
+
+	"toggle_fullscreen": "Toggle Fullscreen On/Off",
+	"toggle_dark_mode": "Toggle Dark Mode On/Off",
 }
 
 
 func _ready() -> void:
+	_setup_initial_state()
 	_connect_signals()
+
+
+func _setup_initial_state() -> void:
+	for child in %Sections.get_children():
+		if child is FoldableContainer:
+			child.folded = true
 
 	var app_version : String = ProjectSettings.get("application/config/version")
 	if app_version:
 		%Version/ID.text = app_version
 	else:
 		%Version.queue_free()
-
-	for action in [
-		"view_mode_1",
-		"view_mode_2",
-		"view_mode_3",
-		"view_mode_4",
-	]:
-		var hint = INPUT_HINT_SCENE.instantiate()
-		hint.description = descriptions[action]
-		hint.key_binding = InputMap.action_get_events(action)[0]
-		%KeyBindingsBlock1.add_child(hint)
-
-	for action in [
-		"shift_view_backward",
-		"previous_day",
-		"jump_to_today",
-		"next_day",
-		"shift_view_forward",
-	]:
-		var hint = INPUT_HINT_SCENE.instantiate()
-		hint.description = descriptions[action]
-		hint.key_binding = InputMap.action_get_events(action)[0]
-		%KeyBindingsBlock2.add_child(hint)
-
-	for action in [
-		"search_screen",
-	]:
-		var hint = INPUT_HINT_SCENE.instantiate()
-		hint.description = descriptions[action]
-		hint.key_binding = InputMap.action_get_events(action)[0]
-		%KeyBindingsBlock3.add_child(hint)
-
-	for action in [
-		"toggle_calendar_widget",
-		"show_help",
-		"show_bookmarks",
-		"show_capture",
-		"show_settings",
-		"toggle_fullscreen",
-		"toggle_dark_mode",
-	]:
-		var hint = INPUT_HINT_SCENE.instantiate()
-		hint.description = descriptions[action]
-		hint.key_binding = InputMap.action_get_events(action)[0]
-		%KeyBindingsBlock4.add_child(hint)
 
 	for action in [
 		"toggle_todo",
@@ -113,7 +77,59 @@ func _ready() -> void:
 		var hint = INPUT_HINT_SCENE.instantiate()
 		hint.description = descriptions[action]
 		hint.key_binding = InputMap.action_get_events(action)[0]
-		%KeyBindingsBlock5.add_child(hint)
+		%Section1/Content.add_child(hint)
+
+	for action in [
+		"view_mode_1",
+		"view_mode_2",
+		"view_mode_3",
+		"view_mode_4",
+	]:
+		var hint = INPUT_HINT_SCENE.instantiate()
+		hint.description = descriptions[action]
+		hint.key_binding = InputMap.action_get_events(action)[0]
+		%Section2/Content.add_child(hint)
+
+	for action in [
+		"shift_view_backward",
+		"previous_day",
+		"jump_to_today",
+		"next_day",
+		"shift_view_forward",
+	]:
+		var hint = INPUT_HINT_SCENE.instantiate()
+		hint.description = descriptions[action]
+		hint.key_binding = InputMap.action_get_events(action)[0]
+		%Section3/Content.add_child(hint)
+
+	for action in [
+		"search_screen",
+	]:
+		var hint = INPUT_HINT_SCENE.instantiate()
+		hint.description = descriptions[action]
+		hint.key_binding = InputMap.action_get_events(action)[0]
+		%Section4/Content.add_child(hint)
+
+	for action in [
+		"toggle_calendar_widget",
+		"show_help",
+		"show_bookmarks",
+		"show_capture",
+		"show_settings",
+	]:
+		var hint = INPUT_HINT_SCENE.instantiate()
+		hint.description = descriptions[action]
+		hint.key_binding = InputMap.action_get_events(action)[0]
+		%Section5/Content.add_child(hint)
+
+	for action in [
+		"toggle_fullscreen",
+		"toggle_dark_mode",
+	]:
+		var hint = INPUT_HINT_SCENE.instantiate()
+		hint.description = descriptions[action]
+		hint.key_binding = InputMap.action_get_events(action)[0]
+		%Section6/Content.add_child(hint)
 
 
 func _connect_signals() -> void:
